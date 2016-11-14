5c6440f9cca617a39b2f840416782354e075a96b
Matias Bruzzo <mabruzzo@udc.edu.ar>
2016-11-14 15:58:28 -0300

    cambios

--- src/main/java/app/models/Usuario.java
+++ src/main/java/app/models/Usuario.java
@@ -5,12 +5,31 @@
  */
 package app.models;
 
+import java.util.List;
 import org.javalite.activejdbc.Model;
+import static org.javalite.activejdbc.Model.findAll;
+import org.javalite.activejdbc.annotations.Table;
 
 /**
  *
- * @author Soraya
+ * @author Matias
  */
+@Table("usuario")
 public class Usuario extends Model{
     
+     public static List lista_usuario() {
+        return findAll();
+    }
+
+    public static boolean crear(Usuario u) {
+        return u.save();
+    }
+
+    public static boolean eliminar(Usuario u) {
+        return u.delete();
+    }
+
+    public static boolean actualizar(Usuario u) {
+        return u.saveIt();
+    }
 }
